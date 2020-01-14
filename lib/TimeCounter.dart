8742b76ed7937b03d117c39f06caa880a777dba7
Olufemi Jolugbo <jolugbofemi@gmail.com>
2020-01-14 11:44:06 +0100

    flutter intermediate stopwatch, including a timecounter,timedisplay & TimeCounterConsumer class

---
+++ lib/TimeCounter.dart
@@ -0,0 +1,80 @@
+import 'package:flutter/material.dart';
+import 'dart:async';
+import 'TimeDisplay.dart';
+
+class TimeCounter extends StatefulWidget {
+  @override
+  _TimeCounterState createState() => new _TimeCounterState();
+}
+
+class _TimeCounterState extends State<TimeCounter> {
+  Stopwatch _watch;
+  Timer _timer;
+  Duration _duration;
+
+  void _onStart() {
+    setState(() {
+      _watch = new Stopwatch();
+      _timer = new Timer.periodic(new Duration(milliseconds: 250), _onTimeOut);
+    });
+    _watch.start();
+  }
+
+  void _onStop() {
+    _timer.cancel();
+    _watch.stop();
+  }
+
+  void _onTimeOut(Timer timer) {
+    if (!_watch.isRunning) return;
+    setState(() {
+      _duration = _watch.elapsed;
+    });
+  }
+
+  void _onClear(Duration value) {
+setState(() {
+  _duration = new Duration();
+});
+  }
+
+  @override
+  void initState() {
+    _duration = new Duration();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return new Container(
+      padding: new EdgeInsets.all(10.0),
+      child: new Center(
+        child: new Column(
+          children: <Widget>[
+            new TimeDisplay(
+              color: Colors.red,
+              duration: _duration,
+              onClear: _onClear,
+            ),
+            new Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  new Padding(
+                    padding: EdgeInsets.all(10.0),
+                    child: RaisedButton(
+                      onPressed: _onStart,
+                      child: Text('Start'),
+                    ),
+                  ),
+                  new Padding(
+                      padding: EdgeInsets.all(10.0),
+                      child: RaisedButton(
+                        onPressed: _onStop,
+                        child: Text("Stop"),
+                      ))
+                ])
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/TimeCounterconsumer.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import 'TimeCounter.dart';
+void main(){
+  runApp(new MaterialApp(
+    home: new MainApp()
+  ));
+}
+class MainApp extends StatefulWidget{
+  @override
+  _State createState() => new _State();
+
+}
+class _State extends State<MainApp>{
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      appBar: new AppBar(
+        title: new Text("Project Flutter"),
+      ),
+      body: new Container(
+        padding: new EdgeInsets.all(32.0),
+        child: new Center(
+          child: new Column(
+            children: <Widget>[
+              new Text("My Stop Watch"),
+              new TimeCounter()
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
